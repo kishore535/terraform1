3d353ab54736a1a6ac04acecc2fd08178bebc13d
Charles Sieg <charlessieg@Charless-MBP.attlocal.net>
2021-04-21 17:31:52 -0500

    Fixes.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "bucket_arn" {
   description = "The ARN of the S3 bucket."
-  value       = aws_s3_bucket.logs.arn
+  value       = aws_s3_bucket.main.arn
 }
 
 output "bucket_domain_name" {
   description = "The bucket domain name of the S3 bucket."
-  value       = aws_s3_bucket.logs.bucket_domain_name
+  value       = aws_s3_bucket.main.bucket_domain_name
 }
 
 output "bucket_id" {
   description = "The ID of the S3 bucket."
-  value       = aws_s3_bucket.logs.id
+  value       = aws_s3_bucket.main.id
 }
 
 output "bucket_name" {
   description = "The computed name of the S3 bucket."
-  value       = aws_s3_bucket.logs.bucket
+  value       = aws_s3_bucket.main.bucket
 }
