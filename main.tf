62e36e53d546c40781b8118d8ee2f12f7892d63e
Charles Sieg <charles.sieg@veranahealth.com>
2021-05-03 15:15:29 -0500

    Added count for policy.

--- main.tf
+++ main.tf
@@ -196,6 +196,7 @@ data "aws_iam_policy_document" "main" {
 
 resource "aws_s3_bucket_policy" "main" {
   bucket     = aws_s3_bucket.main.bucket
+  count      = var.allow_alb_logging || var.allow_cloudfront_logging || var.allow_vpc_flow_logging ? 1 : 0
   policy     = data.aws_iam_policy_document.main.json
   depends_on = [aws_s3_bucket_public_access_block.main]
 }
