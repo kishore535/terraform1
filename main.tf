82a671604c83af6aaab32606189cc2bcef889075
Charles Sieg <charles.sieg@veranahealth.com>
2021-05-18 15:45:35 -0500

    Fixes.

--- main.tf
+++ main.tf
@@ -165,7 +165,7 @@ data "aws_iam_policy_document" "main" {
     for_each = var.allow_vpc_flow_logging ? [1] : []
 
     content {
-      sid       = "AWSLogDeliveryWrite"
+      sid       = "AWSLogDeliveryWriteDevOpsX"
       effect    = "Allow"
       actions   = ["s3:PutObject"]
       resources = ["${aws_s3_bucket.main.arn}/AWSLogs/*"]
